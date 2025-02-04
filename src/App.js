ca399385907c2df281e9c4ce4aa5de546788c9c1
jayp0204 <161871900+jayp0204@users.noreply.github.com>
2025-02-04 10:44:18 +0530

    DSA tracker list

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import DSAList from "./components/DSAList";
+
+export default function App() {
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center mt-4">
+                <h3 className="bg-clip-text text-transparent bg-gradient-to-r 
+                               from-purple-500 to-pink-500 font-bold text-2xl mb-4">
+                    DSA Tracker
+                </h3>
+                <DSAList />
+            </div>
+        </>
+    );
+}
